2d035a5aacea2f97781a41ffe28a99952708f8ec
Lyndon Shi <shilyndon@gmail.com>
2018-07-30 17:35:40 -0400

    use ibm tone analyzer successfully

---
+++ backend/Stock/wrappers/ibm_tone_analyzer.py
@@ -0,0 +1,21 @@
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud.tone_analyzer_v3 import ToneInput
+
+from Stock.wrappers.apiCredentials import api_credentials
+
+
+class IBMToneAnalyzerWrapper:
+    def __init__(self):
+        self.credentials = api_credentials['ibm-tone-analyzer']
+        self.tone_analyzer = ToneAnalyzerV3(
+            version='2018-07-30',
+            iam_api_key=self.credentials['apikey'],
+            url=self.credentials['url']
+        )
+
+    def analyze_sentiment(self, text):
+        tone_input = ToneInput(text)
+        tone = self.tone_analyzer.tone(tone_input, 'application/json')
+
+        print(json.dumps(tone, indent=2))
