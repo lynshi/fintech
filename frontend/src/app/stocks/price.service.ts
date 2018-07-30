72bc65380fd21e98e8545157b2eef99b9977cad4
Lyndon Shi <shilyndon@gmail.com>
2018-07-30 14:21:23 -0400

    display price chart

--- frontend/src/app/stocks/price.service.ts
+++ frontend/src/app/stocks/price.service.ts
@@ -13,14 +13,32 @@ export class PriceService {
 
   constructor(private http: HttpClient) { }
 
-  getStockPrice(symbol: string) : Observable<Stock> {
+  getStockPrice(symbol: string) : Observable<any[]> {
     return this.getStockPriceFromIEX(symbol, '1d').pipe(map(res => {
       let stock : Stock;
       stock = {
         symbol: symbol,
         price: res[res.length - 1]['close']
       };
-      return stock;
+
+      let time_arr = [];
+      let price_arr = [];
+      res.forEach((datum) => {
+        let date = datum['date'];
+        let year = date.substr(0, 4);
+        let month = date.substr(4, 2);
+        let day = date.substr(6, 2);
+
+        let minute = datum['minute'];
+        let time = minute.split(':');
+
+        time_arr.push((new Date(year, month, day, time[0], time[1]))
+          .toLocaleTimeString('en', {
+            year: 'numeric', month: 'short', day: 'numeric' }));
+        price_arr.push(datum['close']);
+      });
+
+      return [stock, time_arr, price_arr];
     }));
   }
 
--- frontend/src/app/stocks/stock-display/price-display/price-display.component.ts
+++ frontend/src/app/stocks/stock-display/price-display/price-display.component.ts
@@ -3,11 +3,12 @@ import {
   Component,
   Input,
   OnInit,
-  ChangeDetectorRef,
+  ChangeDetectorRef
 } from '@angular/core';
 import { Stock } from '../../stock';
 import { PriceService } from '../../price.service';
 import { Observable, of } from 'rxjs';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-price-display',
@@ -16,24 +17,60 @@ import { Observable, of } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PriceDisplayComponent implements OnInit {
-  @Input() stock;
+  @Input() stock: Stock;
   stockSymbol$: Observable<String>;
   stockPrice$: Observable<number>;
-  
+  priceChart: Chart;
+  displayChart = false;
+
   constructor(private priceService: PriceService,
               private cdr: ChangeDetectorRef) { }
 
   ngOnInit() { }
 
   ngOnChanges() {
-    this.getStockPrice();
+    if (this.stock) {
+      this.getStockPrice();
+    }
   }
 
   getStockPrice(): void {
     this.priceService.getStockPrice(this.stock.symbol)
       .subscribe(stockData => {
-        this.stockSymbol$ = of(stockData.symbol);
-        this.stockPrice$ = of(stockData.price);
+        this.stockSymbol$ = of(stockData[0].symbol);
+        this.stockPrice$ = of(stockData[0].price);
+        this.displayChart = true;
+        this.cdr.detectChanges();
+
+        this.priceChart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: stockData[1],
+            datasets: [
+              {
+                data: stockData[2],
+                borderColor: "#3cba9f",
+                fill: false
+              },
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }],
+            },
+            responsive: true,
+            position: "center"
+          }
+        });
+
         this.cdr.detectChanges();
       });
   }
