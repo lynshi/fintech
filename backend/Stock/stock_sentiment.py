5e1b5fc614f6a4d4fb8d4d1f4537c034dd67f0a9
Lyndon Shi <shilyndon@gmail.com>
2018-07-31 15:28:00 -0400

    get sentiment for all found articles

---
+++ backend/Stock/stock_sentiment.py
@@ -0,0 +1,39 @@
+from Stock.wrappers.ibm_tone_analyzer import IBMToneAnalyzerWrapper
+from Stock.wrappers.news_api import NewsAPIWrapper
+
+
+class StockSentiment:
+    def __init__(self):
+        self.ibm_sentiment_analyzer = IBMToneAnalyzerWrapper()
+        self.news_api = NewsAPIWrapper()
+
+    def get_stock_sentiment(self, company_name):
+        news_data = self.news_api.get_news_for_stock(company_name)
+        sentiment_holder = {}
+        sentiment_result = {}
+        for article in news_data['articles']:
+            try:
+                sentiment = self.ibm_sentiment_analyzer.analyze_sentiment(
+                    article['description']
+                )['document_tone']
+            except KeyError:
+                continue
+
+            if len(sentiment['tones']) == 0:
+                continue
+
+            for tone in sentiment['tones']:
+                if tone['tone_id'] not in sentiment_holder:
+                    sentiment_holder[tone['tone_name']] = {'count': 0,
+                                                           'sentiment': 0}
+                sentiment_holder[tone['tone_name']]['count'] += 1
+                sentiment_holder[tone['tone_name']]['sentiment'] \
+                    += tone['score']
+
+        for tone in sentiment_holder:
+            sentiment_result[tone] = {
+                'sentiment': sentiment_holder[tone]['sentiment']
+                             / sentiment_holder[tone]['count']
+            }
+
+        return sentiment_result
--- backend/test.py
+++ backend/test.py
@@ -1,14 +1,7 @@
 import json
 
-from Stock.wrappers.ibm_tone_analyzer import IBMToneAnalyzerWrapper
-from Stock.wrappers.news_api import NewsAPIWrapper
+from Stock.stock_sentiment import StockSentiment
 
-ibm = IBMToneAnalyzerWrapper()
-sentiment = ibm.analyze_sentiment('Hi, today was a great day! I really enjoyed eating ice cream.')
-print(json.dumps(sentiment, indent=4))
-input()
-
-news = NewsAPIWrapper()
-data = news.get_news_for_stock('microsoft')
-print(json.dumps(data, indent=4))
-print(type(data))
+s = StockSentiment()
+sentiment = s.get_stock_sentiment('microsoft')
+print(json.dumps(sentiment, indent=4, sort_keys=True))
