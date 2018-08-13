72df5a303864493d6bf629663117caae172bb5b9
Lyndon Shi <shilyndon@gmail.com>
2018-08-13 17:24:06 -0400

    select sources

--- backend/Stock/wrappers/news_api.py
+++ backend/Stock/wrappers/news_api.py
@@ -4,16 +4,27 @@ from Stock.wrappers.apiCredentials import api_credentials
 
 
 class NewsAPIWrapper:
+    SOURCES = ['bloomberg', 'business-insider', 'financial-post',
+               'financial-times', 'reuters', 'the-wall-street-journal', 'cnbc']
+
     def __init__(self):
         self.credentials = api_credentials['news-api']
         self.url = self.credentials['url']
 
-    def get_news_for_stock(self, company):
+    def get_news_for_stock(self, company, from_date=datetime.datetime.today()):
         company = company.replace(' ', '%20')
         request_url = self.url + 'everything?' + \
-                      'q=' + company + '%20stock%20news' + '&' + \
+                      'q=' + company + '&' + \
                       'from=' + \
-                      datetime.datetime.today().strftime('%Y-%m-%d') + \
+                      from_date.strftime('%Y-%m-%d') + '&' + \
                       'sortBy=popularity&' + \
-                      'apiKey=' + self.credentials['apikey']
+                      'sources='
+
+        for s in NewsAPIWrapper.SOURCES:
+            request_url += s + ','
+
+        request_url += '&apiKey=' + self.credentials['apikey']
+
+        print(requests.get(request_url).json())
+
         return requests.get(request_url).json()
